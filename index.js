f73abfeec70ed5491b085e03fea7738b151c7715
Stephanie Fu <fus@mit.edu>
2018-07-21 13:55:36 -0500

    Add Mapbox geocoder

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var path = require('path');
 
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res) {
--- public/main.js
+++ public/main.js
@@ -3,13 +3,14 @@ var CLIENT_SECRET = config.CLIENT_SECRET;
 var MAPS_API_KEY = config.MAPS_API_KEY;
 
 function initMap() {
-    // The location of Uluru
-    //var uluru = {lat: -25.344, lng: 131.036};
-    // The map, centered at Uluru
-    // The marker, positioned at Uluru
-    //var marker = new google.maps.Marker({position: uluru, map: map});
-    //initAutocomplete(map);
     startMapbox();
+}
+
+function addSearch(map) {
+  console.log("adding search");
+  map.addControl(new MapboxGeocoder({
+    accessToken: MAPS_API_KEY
+  }));
 }
 
 function startMapbox() {
@@ -21,9 +22,10 @@ function startMapbox() {
       zoom: 8
     });
 
+    addSearch(map);
     map.on('click', function(e) {
       var features = map.queryRenderedFeatures(e.point, {
-        layers: ['kc-locations'] // replace this with the name of the layer
+        layers: ['kc-locations']
       });
     
       if (!features.length) {
@@ -52,104 +54,6 @@ function startMapbox() {
         
         map.flyTo({ center: [feature.geometry.coordinates[0], feature.geometry.coordinates[1]] });
     });
+
+    var findRecommendationButton = document.getElementById('submit-query-button');
 }
-
-/*function initAutocomplete(map) {
-
-    // Create the search box and link it to the UI element.
-    var input = document.getElementById('pac-input');
-    var searchBox = new google.maps.places.SearchBox(input);
-    var submitButton = document.getElementById('submit-query-button');
-    // Bias the SearchBox results towards current map's viewport.
-    map.addListener('bounds_changed', function() {
-      searchBox.setBounds(map.getBounds());
-    });
-
-    var markers = [];
-    // Listen for the event fired when the user selects a prediction and retrieve
-    // more details for that place.
-    submitButton.addEventListener('click', function() {
-      var places = searchBox.getPlaces();
-
-      if (places.length == 0) {
-        console.log("nothing");
-        return;
-      }
-
-      // Clear out the old markers.
-      markers.forEach(function(marker) {
-        marker.setMap(null);
-      });
-      markers = [];
-
-      // For each place, get the icon, name and location.
-      var bounds = new google.maps.LatLngBounds();
-      places.forEach(function(place) {
-        if (!place.geometry) {
-          console.log("Returned place contains no geometry");
-          return;
-        }
-        var latlng = place.geometry.location;
-        var icon = {
-          url: place.icon,
-          size: new google.maps.Size(71, 71),
-          origin: new google.maps.Point(0, 0),
-          anchor: new google.maps.Point(17, 34),
-          scaledSize: new google.maps.Size(25, 25)
-        };
-
-        var lat = place.geometry.location.lat();
-        var lng = place.geometry.location.lng();
-
-        var query = document.getElementById('query-input').value;
-        console.log('here');
-        var fourSquareLink = 'https://api.foursquare.com/v2/venues/explore/?' + 
-        'll=' + lat + ',' + lng + '&' + 
-        'limit=100&' + 
-        'query=' + query + '&' + 
-        'client_id=' + CLIENT_ID + '&' + 
-        'client_secret=' + CLIENT_SECRET +
-        '&v=20180719';
-        console.log(fourSquareLink);
-        $.getJSON(fourSquareLink,
-            function (data) {
-                console.log('here2');
-                console.log(data);
-                $.each(data.response.groups[0].items, function (i, venue) {
-                    var venueLat = venue.venue.location.lat;
-                    var venueLng = venue.venue.location.lng;
-                    content = 'Name: ' + venue.venue.name +
-                                ' Address: ' + venue.venue.location.address +
-                                ' Lat/long: ' + venueLat + ', ' + venueLng + '\n';
-                    console.log(content);
-
-                    markers.push(new google.maps.Marker({
-                        map: map,
-                        icon: icon,
-                        title: venue.venue.name,
-                        position: {lat: venueLat, lng: venueLng}
-                    }));
-                });
-            });
-        // Create a marker for each place.
-
-        //start comment out
-        markers.push(new google.maps.Marker({
-          map: map,
-          icon: icon,
-          title: place.name,
-          position: place.geometry.location
-        }));
-        //end comment out
-
-        if (place.geometry.viewport) {
-          // Only geocodes have viewport.
-          bounds.union(place.geometry.viewport);
-        } else {
-          bounds.extend(place.geometry.location);
-        }
-      });
-      map.fitBounds(bounds);
-    });
-  }
-  */
