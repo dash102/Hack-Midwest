fc4c111db0d8c89425227e318cf2ac65c3deb776
ehiggins98 <ericdhiggins@gmail.com>
2018-07-21 18:19:22 -0500

    Fixed gitignore

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
 var box = require('./box');
 var twilio = require('./twilio');
 
@@ -15,3 +18,13 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 module.exports = app;
+
+io.on('connection', function(socket) {
+    console.log('a user connected');
+    socket.on('disconnect', function() {
+        console.log('user disconnected');
+    });
+    socket.on('chat message', function(msg) {
+        console.log('message: ' + msg);
+    });
+});
