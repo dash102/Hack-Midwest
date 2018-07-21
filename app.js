63c680546fcb3ff62b5fde07092f8cdbfbb6c6d6
ehiggins98 <ericdhiggins@gmail.com>
2018-07-21 18:12:13 -0500

    Finished fixing for EB

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+var box = require('./box');
+var twilio = require('./twilio');
 
 var app = express();
 
@@ -12,7 +13,5 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
 
 module.exports = app;
