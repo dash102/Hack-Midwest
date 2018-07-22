39030f51186131c9076da69894fcf82894c11a5a
ehiggins98 <ericdhiggins@gmail.com>
2018-07-22 09:07:24 -0500

    Slowly trying stuff

--- app.js
+++ app.js
@@ -57,6 +57,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.get('/view', async (req, res) => res.send(await view.render(req.query['id'])));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
